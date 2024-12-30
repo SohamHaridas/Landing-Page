5ec98dd9068feeb8b63f8a1e56c71ec20ed5175b
SohamHaridas <162135189+SohamHaridas@users.noreply.github.com>
2024-12-30 20:02:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const ctaButton = document.getElementById('cta-btn');
+ctaButton.addEventListener('click', () => {
+    alert('Thank you for choosing ExploreWorld! We will help you plan your trip.');
+});
